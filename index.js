2f6b8c06b72fe1d581d40247e0df858fc2ec00a3
projectsdatadna <107699067+projectsdatadna@users.noreply.github.com>
2022-10-06 15:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("SERVER RUNNING 5000");
+server.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port 5000", this.address().port, app.settings.env);
 });
